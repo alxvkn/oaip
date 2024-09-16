4effbb2b4e1cf5d994fb4c06998f49b76f478158
on8t <64454213+on8t@users.noreply.github.com>
2024-09-16 12:35:34 +0300

    2024-09-16: задание 1

--- 2024-09-16/1.cpp
+++ 2024-09-16/1.cpp
@@ -8,5 +8,9 @@ int main(void) {
 
     discount = (cost > 500 ? (cost > 1000 ? 0.05 : 0.03) : 0);
 
+    std::cout << "Стоимость покупки со скидкой: "
+        << cost - (cost * discount)
+        << std::endl;
+
     return 0;
 }
