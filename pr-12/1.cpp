4cba1346e485a78992aa290871b79f2d51ae09f0
on8t <64454213+on8t@users.noreply.github.com>
2024-10-29 10:34:34 +0300

    pr-12

---
+++ pr-12/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char *argv[]) {
+    int h, m;
+    cin >> h >> m;
+
+    cout << 12 - h << " " << 60 - m << endl;
+
+    return 0;
+}
